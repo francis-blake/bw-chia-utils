207354a5209487990acf18310aeb9531bfc1a840
João Santos <joao.santos@bway.pt>
2023-04-19 08:29:45 +0100

    working version - register-plot-rate

--- register-plot-rate/register-plot-rate.js
+++ register-plot-rate/register-plot-rate.js
@@ -5,11 +5,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const fs = require("fs");
 const https = require("https");
-// const qfgets = require("qfgets");
+const qfgets = require("qfgets");
 const { exec } = require("child_process");
 const api_host = process.env.API_HOST;
 const login_uri = process.env.API_LOGIN_URI;
-const plot_manager_uri = process.env.PLOT_MANAGER_URI;
 const plot_manager_update_uri = process.env.PLOT_MANAGER_UPDATE_URI;
 let api_token = "";
 let logged_in = 0;
@@ -22,65 +21,158 @@ const chia_bin = process.env.CHIA_BIN;
 const device_name = os.hostname();
 
 let tempList = [];
+let proofsRate;
 
 let plotList = require("./plot-list.json");
+const totalPlotList = plotList.length;
+let processed = 0;
 
-plotList.forEach((p) => {
-  if (tempList.length > 10) {
-    sendToServer();
+sendAllPlots();
+
+async function sendAllPlots() {
+  for (const pl of plotList) {
+    let rate = await getPlotRate(pl);
+    let newP = {
+      id: pl,
+      rate: rate,
+    };
+    processPlot(newP);
+    return;
   }
+}
 
-  getPlotRate(p).then((r) => {
-    console.log(0, r);
-  });
+function processPlot(p) {
+  console.log("adding to temporary list:", p);
   tempList.push(p);
-});
+  const index = plotList.indexOf(p.id);
+  const x = plotList.splice(index, 1);
 
-function sendToServer() {
-  console.log(tempList);
+  processTempList();
+  processed++;
 }
 
-function getPlotRate(p) {
-  exec(
-    "mmchia " +
-      " plots check -g " +
-      p.id +
-      " > logs/retrieving_proofs.log 2>&1",
-    (err, stdout, stderr) => {
-      if (err) {
-        let log = fs.createWriteStream("logs/plots_resumo_errors.log", {
-          flags: "a",
-        });
-        log.write(" could not get proofs of " + plotToSend.id + "\n");
-        // node couldn't execute the command
-        return;
+function processTempList() {
+  if (tempList.length > 9 || totalPlotList == processed + 1) {
+    const listToSend = tempList;
+    tempList = [];
+
+    console.log("Sending to server:", listToSend);
+    sendToServer(listToSend);
+  }
+}
+
+async function getPlotRate(p) {
+  return new Promise((resolve, reject) => {
+    exec(
+      "cd " +
+        home_folder +
+        "/bw-chia-utils/register-plot-rate; " +
+        chia_bin +
+        " plots check -g " +
+        p,
+      (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+          let log = fs.createWriteStream("logs/plots_resumo_errors.log", {
+            flags: "a",
+          });
+          log.write(" could not get proofs of " + actualPlot.id + "\n");
+          // node couldn't execute the command
+          // actualPlot.rate = null;
+          return;
+        }
+        resolve(processResult(stderr));
       }
-
-      grepWithFs("logs/retrieving_proofs.log", "Proofs ", processRate);
-    }
-  );
+    );
+  });
 }
 
-function grepWithFs(filename, regexp, done) {
-  fp = new qfgets(filename, "r");
-  function loop() {
-    for (i = 0; i < 40; i++) {
-      line = fp.fgets();
-      if (line && line.match(regexp)) proofsRate = line;
+var processResult = function (stdout) {
+  let rate;
+  var lines = stdout.toString().split("\n");
+  var results = new Array();
+  lines.forEach(function (line) {
+    if (line && line.match("Proofs")) {
+      rate = parseInt(line.split(" ")[17]) / 30;
+      rate = Math.round(rate * 10000) / 10000;
     }
-    if (!fp.feof()) setImmediate(loop);
-    else done();
-  }
-  loop();
+  });
+
+  return rate;
+};
+
+function sendToServer(l) {
+  // start sending
+  let sending = setInterval(function () {
+    if (logged_in < 2) {
+      postRequest(plot_manager_update_uri, JSON.stringify(l)).then((d) => {
+        let r = JSON.parse(d);
+        if (r.success >= 0 || r.already >= 0) {
+          // logIt(l.length, r);
+          clearInterval(sending);
+          logged_in = 0;
+        }
+      });
+    } else {
+      clearInterval(sending);
+    }
+  }, 2500);
 }
 
-function processRate() {
-  if (proofsRate) {
-    let rate = parseInt(proofsRate.split(" ")[17]) / 30;
-    rate = Math.round(rate * 10000) / 10000;
-    plotToSend.rate = rate;
-    console.log("last plot rate: ", plotToSend.rate);
+function login() {
+  if (login.pending) return login.pending;
+  const data = JSON.stringify({
+    email: api_user,
+    password: api_password,
+    device_name: device_name,
+  });
 
-    return rate;
-  }
+  login.pending = postRequest(login_uri, data).then((d) => {
+    api_token = JSON.parse(d);
+    logged_in = 1;
+    delete login.pending;
+  });
+}
+
+function postRequest(uri, body) {
+  const options = {
+    hostname: api_host,
+    port: api_port,
+    path: uri,
+    headers: {
+      "Content-Type": "application/json",
+      "X-Requested-With": "XMLHttpRequest",
+      Authorization: "Bearer " + api_token,
+    },
+    method: "POST",
+  };
+
+  var resolve, reject, data;
+  var promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+
+  const req = https.request(options, (res) => {
+    res.on("data", (d) => {
+      data = d;
+      logged_in = 2;
+    });
+
+    res.on("end", () => {
+      if (res.statusCode == 401) {
+        login();
+      }
+      resolve(data);
+    });
+  });
+  //handling error
+  req.on("error", (e) => {
+    reject("problem with request: " + e.message);
+  });
+
+  req.write(body);
+  req.end();
+
+  return promise;
 }
